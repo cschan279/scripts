8c918e034e74bd4c62941106ca41d7784d902740
Troy Chan <42487786+cschan279@users.noreply.github.com>
2020-05-01 18:00:15 +0800

    Update activate_virtualenv.sh

--- activate_virtualenv.sh
+++ activate_virtualenv.sh
@@ -2,13 +2,16 @@
 
 # copy it into .bashrc
 function activate() {
-	if [ -z "$1" ]
-		then
-		    en="/home/"$USER"/venv/venv/bin/activate"
-	else
-		en="/home/"$USER"/venv/"$1"/bin/activate"
-		
-	fi
-	echo $en
-	source $en
+  if [ -z "$1" ]
+    then
+      en="/home/"$USER"/venv/venv/bin/activate"
+  else
+    en="/home/"$USER"/venv/"$1"/bin/activate"
+  fi
+  echo $en
+  source $en
+}
+
+function tfcheckgpu() {
+    python -c "import tensorflow as tf;print(tf.config.list_physical_devices('GPU'));"
 }

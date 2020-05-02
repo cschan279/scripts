70bd8d5cbe8690b6c29cfcbf876f508f0d960abf
Troy Chan <42487786+cschan279@users.noreply.github.com>
2020-05-02 19:40:05 +0800

    Create gsettings.sh

---
+++ sh/gsettings.sh
@@ -0,0 +1,5 @@
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
+gsettings set org.gnome.Vino require-encryption false
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
